d354e8e026438b207d248d4c22044c0d2acbbce8
Matthew Cragg <matthewlcragg@gmail.com>
2023-06-19 13:31:36 +0100

    FileGeneration

--- RATClient/Client.cs
+++ RATClient/Client.cs
@@ -1,27 +1,42 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
-//Create new TCP/IP request for remote access
-IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
-Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+internal class Program
+{
+    private static Stream fullPath;
 
-//Connect to remote server
-socket.Connect(iPEndPoint);
+    private static void Main(string[] args)
+    {
+        //Create new TCP/IP request for remote access
+        IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-//Send commands to the remote server
-string command = "Connected to first server";
-byte[] commandBuffer = Encoding.ASCII.GetBytes(command);
-socket.Send(commandBuffer);
+        //Connect to remote server
+        socket.Connect(iPEndPoint);
 
-//Get response from server
-byte[] responseBuffer = new byte[1024];
-int receivedBytes = socket.Receive(responseBuffer);
-string response = Encoding.ASCII.GetString(responseBuffer, 0, receivedBytes);
+        //Send commands to the remote server
+        string command = "Connected to first server";
+        byte[] commandBuffer = Encoding.ASCII.GetBytes(command);
+        socket.Send(commandBuffer);
 
-//Display response
-Console.WriteLine("response");
-Console.ReadLine();
+        using (var file = File.Create("pricequote.txt")) ;
 
-//Close socket
-socket.Close();
+
+            //Get response from server
+            byte[] responseBuffer = new byte[1024];
+        int receivedBytes = socket.Receive(responseBuffer);
+        string response = Encoding.ASCII.GetString(responseBuffer, 0, receivedBytes);
+
+        //Display response
+        Console.WriteLine("response");
+        Console.ReadLine();
+
+        //Close socket
+        socket.Close();
+    }
+}
