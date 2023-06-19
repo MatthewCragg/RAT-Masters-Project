2fe659e06a7b4308048fcabc7fdd3a615bd44e61
Matthew Cragg <matthewlcragg@gmail.com>
2023-06-19 11:05:11 +0100

    FirstIteration

---
+++ RATClient/Client.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+//Create new TCP/IP request for remote access
+IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+//Connect to remote server
+socket.Connect(iPEndPoint);
+
+//Send commands to the remote server
+string command = "Connected to first server";
+byte[] commandBuffer = Encoding.ASCII.GetBytes(command);
+socket.Send(commandBuffer);
+
+//Get response from server
+byte[] responseBuffer = new byte[1024];
+int receivedBytes = socket.Receive(responseBuffer);
+string response = Encoding.ASCII.GetString(responseBuffer, 0, receivedBytes);
+
+//Display response
+Console.WriteLine("response");
+Console.ReadLine();
+
+//Close socket
+socket.Close();
---
+++ RATServer/Server.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+//Accept any IP address running on Port 8080, and new create new TCP/IP socket for remote access
+IPEndPoint IPEndpoint = new IPEndPoint(IPAddress.Any, 8080);
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Bind(IPEndpoint);
+
+//Listen for any incoming client requests - Listen number is how many clients can connect
+socket.Listen(5);
+Socket clientSocket = socket .Accept();
+
+//Recieve data automatically from remote user
+
+byte[] buffer = new byte[1024];
+int recieveBytes = clientSocket.Receive(buffer);
+
+//Process and decode recieved data
+string recieveData = Encoding.ASCII.GetString(buffer, 0, recieveBytes);
+
+//Execute any recieved command
+Console.WriteLine(recieveData);
+
+//Send response to remote user
+String response = "command Successfully Executed";
+byte[] responseBuffer = Encoding.ASCII.GetBytes(response);
+clientSocket.Send(responseBuffer);
+
+Console.ReadLine();
+
+//Close server
+socket.Close();
